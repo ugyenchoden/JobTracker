61a407b246aa5545e9d6c7a307c3e930f0770e82
ugyenchoden <cugyen20@gmail.com>
2026-07-14 13:51:20 +1000

    models set up

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_14_033405) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_14_034426) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "applications", force: :cascade do |t|
+    t.date "applied_on", null: false
+    t.bigint "company_id", null: false
+    t.datetime "created_at", null: false
+    t.string "position", default: "", null: false
+    t.string "status", default: "applied", null: false
+    t.datetime "updated_at", null: false
+    t.text "url", default: "", null: false
+    t.bigint "user_id", null: false
+    t.index ["applied_on"], name: "index_applications_on_applied_on"
+    t.index ["company_id"], name: "index_applications_on_company_id"
+    t.index ["user_id", "status"], name: "index_applications_on_user_id_and_status"
+    t.index ["user_id"], name: "index_applications_on_user_id"
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name", default: "", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_companies_on_name", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
@@ -25,4 +49,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_14_033405) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "applications", "companies"
+  add_foreign_key "applications", "users"
 end
